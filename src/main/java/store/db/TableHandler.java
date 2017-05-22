9fd1ca40b9360c67e55f9c99773f3894ea7da0ae
constantin-p <constantin.panaitescu@yahoo.com>
2017-05-22 23:17:42 +0200

    fix MySQLIntegrityConstraintViolationException

--- src/main/java/store/db/TableHandler.java
+++ src/main/java/store/db/TableHandler.java
@@ -1,7 +1,7 @@
 package store.db;
 
 
-import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import org.apache.commons.lang3.StringUtils;
 
 import java.sql.Connection;
